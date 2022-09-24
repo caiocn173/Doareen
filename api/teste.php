c7fa03e296a431f68a5d97538f56242ff1435040
caiocn173 <caiocnk173@gmail.com>
2022-09-24 18:17:37 -0300

    Teste Banco de Dados

--- api/teste.php
+++ api/teste.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "sql203.epizy.com";
+/*$servername = "sql203.epizy.com";
 $username = "epiz_32661291";
 $password = "D6UrJcFFcxgW";
 $dbName = "doareen";
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbName);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+}*/
 echo "Connected successfully";
 
 ?>
