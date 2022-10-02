275eac470fa55996d8753035551d7f2f67e3b4a7
caiocn173 <caiocnk173@gmail.com>
2022-10-02 17:02:27 -0300

    Exibir imagem na tela de anuncio

--- api/exibirImagem.php
+++ api/exibirImagem.php
@@ -5,7 +5,7 @@ if(isset($_FILES['file']['name'])){
     $filename = $_FILES['file']['name'];
     
     // Location
-    $location = 'img_exibicao_prod/'.$filename;
+    $location = 'https://doareen.herokuapp.com/Doareen/img_exibicao_prod/'.$filename;
  
     // file extension
     $file_extension = pathinfo($location, PATHINFO_EXTENSION);
