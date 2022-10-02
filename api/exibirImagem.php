615e787b563bf597408f3241fc4d9d70620c8de7
caiocn173 <caiocnk173@gmail.com>
2022-10-02 19:16:01 -0300

    Teste

--- api/exibirImagem.php
+++ api/exibirImagem.php
@@ -8,6 +8,7 @@ if(isset($_FILES['file']['name'])){
     $parent = dirname(__DIR__);
     $location = $parent . '\\img_exibicao_prod\\'.$filename;
     chmod('img_exibicao_prod/', 0777);
+    file_put_contents("php://stderr", $parent .PHP_EOL);
  
     // file extension
     $file_extension = pathinfo($location, PATHINFO_EXTENSION);
