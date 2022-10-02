0d149321aecba8e48f673f2211c5b065cc87e02f
caiocn173 <caiocnk173@gmail.com>
2022-10-02 16:21:24 -0300

    Erro Login

--- api/funcoes.php
+++ api/funcoes.php
@@ -64,7 +64,7 @@ class funcoes extends database{
 
         }
 
-        $string = substr($string, 0,  -3);
+        $string = substr($string, 0,  -2);
         $string .= "')";
 
         $sql = "INSERT INTO clientes(nome_cliente, cpf_cliente, email_cliente, data_nasc_cliente, cep_cliente, cidade_cliente, uf_cliente, bairro_cliente, rua_cliente, numero_cliente, complemento_cliente, telefone_cliente, senha_cliente) VALUES $string;";
