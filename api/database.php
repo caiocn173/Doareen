7939077b514689c3a51be7d1151c82c5e17aef0a
caiocn173 <caiocnk173@gmail.com>
2022-10-02 15:49:38 -0300

    Teste BD

--- api/database.php
+++ api/database.php
@@ -3,19 +3,16 @@
 class database {
 	var $x;
 	function __construct() {
-		$this->x=mysqli_connect("localhost","root","75489873","doareen");
-		
-		mysqli_set_charset($this->x,"utf8");
-		
-
+		$this->x=pg_connect("host=ec2-23-23-151-191.compute-1.amazonaws.com dbname=d96t37t1e8l25b user=jlnrrtrcncxdri password=ed6ead83f589868ba10d5c1dd33199883daaaad7f50eadb48f9935d3819d13a4") or
+		die ("Não foi possível conectar ao servidor PostGreSQL");	
 	}
 	
 	
 	function __destruct() {
-		@mysqli_close($this->x);
+		@pg_close($this->x);
 	}
 	function query($q) {
-		return(mysqli_query($this->x,$q));
+		return(pg_query($this->x,$q));
 	}
 	
 }
