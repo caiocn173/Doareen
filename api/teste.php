fcb828a40dba277935da8c95b4f1fba6568185d1
caiocn173 <caiocnk173@gmail.com>
2022-10-02 12:24:01 -0300

    Teste BD

--- api/teste.php
+++ api/teste.php
@@ -1,9 +1,25 @@
 <?php
 
-$dbHost = 'sql10.freemysqlhosting.net';
-$dbUsername = 'sql10522028';
-$dbPassword = '2qTSZeEq1S';
-$dbName = 'sql10522028';
+/*$host="localhost";
+$port=3306;
+$socket="";
+$user="root";
+$password="";
+$dbname="sakila";
+
+try {
+    $dbh = new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $password));
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+*/
+
+
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '75489873';
+$dbName = 'doareen';
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
