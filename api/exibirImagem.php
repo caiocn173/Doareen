1be0f32b49bb0915a7be4c43f5d763c1e77edc1d
caiocn173 <caiocnk173@gmail.com>
2022-10-02 16:57:28 -0300

    Exibir imagem na tela de anuncio

--- api/exibirImagem.php
+++ api/exibirImagem.php
@@ -5,7 +5,7 @@ if(isset($_FILES['file']['name'])){
     $filename = $_FILES['file']['name'];
     
     // Location
-    $location = '/Doareen/img_exibicao_prod/'.$filename;
+    $location = '../Doareen/img_exibicao_prod/'.$filename;
  
     // file extension
     $file_extension = pathinfo($location, PATHINFO_EXTENSION);
